3e795d661d6a5fd913a2fb1a55e052e30e3b40c6
jiangzongwei <zongwei.jiang01@hand-china.com>
2019-02-16 12:51:05 +0800

    v2.0.3 配合vue项目修改

---
+++ src/main/java/com/habi/boot/system/auth/mapper/SysFunctionMapper.java
@@ -0,0 +1,12 @@
+package com.habi.boot.system.auth.mapper;
+
+
+
+
+import com.habi.boot.system.auth.entity.SysFunctionEntity;
+import com.habi.boot.system.base.mapper.Mapper;
+
+@org.apache.ibatis.annotations.Mapper
+public interface SysFunctionMapper extends Mapper<SysFunctionEntity> {
+
+}
